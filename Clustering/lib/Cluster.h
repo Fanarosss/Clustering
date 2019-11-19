7f109b69d15ad8cd33d485f6ecde930a4afc6f4c
Fanarosss <theof.aslanidis@gmail.com>
2019-11-19 12:31:53 +0200

    rebase

---
+++ Clustering/lib/Cluster.h
@@ -0,0 +1,20 @@
+#include "Initializers.h"
+#include "Assigners.h"
+#include "Updaters.h"
+
+using namespace std;
+
+template <class Point>
+class Cluster {
+private:
+    Initializer<Point>* initializer;
+    Assigner<Point>* assigner;
+    Updater<Point>* updater;
+    int K;
+    vector<int>* centroids;
+    vector<int>** clusters;
+public:
+    Cluster(int, string, string, string);
+    void fit(vector<vector<Point>>*);
+    ~Cluster();
+};
